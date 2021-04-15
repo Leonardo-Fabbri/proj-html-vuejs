87e6e27fac1c04dd0ad9580aa0fe9b3d539b0edb
Leonardo <leonardofabbri125@gmail.com>
2021-04-15 11:51:43 +0200

    aggiunte a JS

--- javascript.js
+++ javascript.js
@@ -10,7 +10,31 @@ var app = new Vue ({
             {message: "Contact"},
             {message: "FREE QUOTE"}
         ],
+        listaBis: [
+            {text:"Home"},
+            {text:"Rates"},
+            {text:"Testimonials"},
+            {text:"Blog"},
+            {text:"Free Quote"}
+        ],
+        listaTRIS: [
+            {textTRIS:"Heading Out To College?"},
+            {textTRIS:"Moving Your Business?"},
+            {textTRIS:"Outstanding Quality"},
+            {textTRIS:"Cost of Moving"},
+            {textTRIS:"Best Moving Tips"},
+
+                
+
+        ],
         button: [
+            {Btext:"ciao"},
+            {BItext:"come va?"},
+            {BIItext:"OK"},
+            {BIIItext:"NO"},
+            {BIVtext:"SI"},
+            {BVtext:"DOVE"},
+            {BVItext:"QUANDO"}
             
         ]
 
